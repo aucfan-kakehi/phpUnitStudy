b55f150424d991502b891b3e9189ad5e3de6d873
kaki <kakehi@aucfan.com>
2016-09-11 12:32:36 +0900

    FuelPHPの設定

---
+++ fuel/app/logs/2016/09/11.php
@@ -0,0 +1,34 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+INFO - 2016-09-11 12:06:14 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = ""
+INFO - 2016-09-11 12:06:14 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:06:14 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:06:21 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:06:21 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:06:21 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2016-09-11 12:06:21 --> Fatal Error - Method Fuel\Core\Date::__toString() must not throw an exception in /mnt/fuelphp/fuel/app/classes/controller/check.php on line 0
+INFO - 2016-09-11 12:07:26 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:07:26 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:07:26 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:07:35 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:07:35 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:07:35 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:07:50 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:07:50 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:07:50 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:08:58 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:08:58 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:08:58 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2016-09-11 12:08:58 --> Fatal Error - Method Fuel\Core\Date::__toString() must not throw an exception in /mnt/fuelphp/fuel/app/classes/controller/check.php on line 0
+INFO - 2016-09-11 12:09:20 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:09:20 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:09:20 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:10:59 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:10:59 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:10:59 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:20:14 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:20:14 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:20:14 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-09-11 12:20:16 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
+INFO - 2016-09-11 12:20:16 --> Fuel\Core\Request::execute - Called
+INFO - 2016-09-11 12:20:16 --> Fuel\Core\Request::execute - Setting main Request
